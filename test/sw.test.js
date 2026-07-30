05ef6281881c2de7b2af0841c01080faaa474e6c
abbaselotfi <167691975+abbaselotfi@users.noreply.github.com>
2026-07-30 03:57:51 +0000

    Update ADA 2026 conversion: test/sw.test.js

--- test/sw.test.js
+++ test/sw.test.js
@@ -12,13 +12,14 @@
 const path = require("path");
 
 const SW = path.join(__dirname, "..", "sw.js");
-const CACHE_NAME = "diabeto-v2";
+const CACHE_NAME = "diabeto-v3";
 const CORE_ASSETS = [
   "./",
   "./index.html",
   "./styles.css",
   "./app.js",
   "./clinical-engine.js",
+  "./soliqua.html",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png",
